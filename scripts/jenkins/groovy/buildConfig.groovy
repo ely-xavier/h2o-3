6af46246b01c24251e482dbf04377e287e858e60
Michal Raška <michalr@h2o.ai>
2018-04-19 10:00:57 +0200

    [ITA-282] Remove pre-installed xgboost from docker image. (#2321)
    * [ITA-282] Remove pre-installed xgboost from docker image.
    
    * Update SHA
    
    (cherry picked from commit 4ecbebb060119e96a112362973c3f8f1332e085c)

--- scripts/jenkins/groovy/buildConfig.groovy
+++ scripts/jenkins/groovy/buildConfig.groovy
@@ -53,7 +53,7 @@ class BuildConfig {
 
   private static final Map EXPECTED_IMAGE_VERSIONS= [
           (DEFAULT_IMAGE): 'docker.h2o.ai/opsh2oai/h2o-3-runtime@sha256:d7bee6340ab06a2e30fa607ba6814f7f6d4d0f81f566fda513e6b700b5cc0b9c',
-          (BENCHMARK_IMAGE): 'docker.h2o.ai/opsh2oai/h2o-3-benchmark@sha256:1edd212621bb8a9e6469c450a86c5136cecdc5d5371179a6e9b513b032dbc182',
+          (BENCHMARK_IMAGE): 'docker.h2o.ai/opsh2oai/h2o-3-benchmark@sha256:23372a462c103fe74f882bf42c0118f44d1d1cda913f6c6038eba1fc70c68990',
 
           'docker.h2o.ai/opsh2oai/h2o-3-hadoop-hdp-2.2:46': 'docker.h2o.ai/opsh2oai/h2o-3-hadoop-hdp-2.2@sha256:a699fc2b06bec2f6b5f180f318b11d70d61c05b0c1fcb8f992ee5e159001c423',
           'docker.h2o.ai/opsh2oai/h2o-3-hadoop-hdp-2.3:46': 'docker.h2o.ai/opsh2oai/h2o-3-hadoop-hdp-2.3@sha256:46a012a791ef31cdc604b7e630c9ab3cdf6247fdaba27c62d45af36ba201d8c3',
